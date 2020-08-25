e8a15f60bc9d60919a443cb76634316cc732a695
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-08-25 13:22:45 +0100

    first commit

---
+++ bench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+rm -rf public/
+
+rm -rf pages/index/index.so
+
+rm -rf pages/index_*
+
+for i in {1..9}; do
+  jamgo new page index_$i
+  cp pages/index/index.go pages/index_$i/index_$i.go
+  cp pages/index/index.gohtml pages/index_$i/index_$i.gohtml
+done
+
+{
+  time jamgo build
+} 2>out.txt
---
+++ go.mod
@@ -0,0 +1,5 @@
+module jamgo-benchmark
+
+go 1.15
+
+require github.com/SamtheSaint/jamgo v0.3.4
