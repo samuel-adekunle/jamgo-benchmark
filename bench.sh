6381e5a1d4dacde76676c040bc1e3ebdda34bcea
Samuel Adekunle <ebunsamuel@yahoo.com>
2020-08-25 13:52:26 +0100

    boilerplate code

--- bench.sh
+++ bench.sh
@@ -8,7 +8,7 @@ rm -rf pages/index/index.so
 
 rm -rf pages/index_*
 
-for i in {1..9}; do
+for i in {1..99}; do
   jamgo new page index_$i
   cp pages/index/index.go pages/index_$i/index_$i.go
   cp pages/index/index.gohtml pages/index_$i/index_$i.gohtml
